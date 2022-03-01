5ff596ace30232514261a35e719543009f9585b3
lekoal <coreglus@gmail.com>
2022-03-01 16:09:28 +0300

    Themes and styles is added

--- app/src/main/java/com/example/nasapicture/ui/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/BottomNavigationDrawerFragment.kt
@@ -1,13 +1,10 @@
 package com.example.nasapicture.ui
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.nasapicture.R
 import com.example.nasapicture.databinding.NavigationDrawerLayoutBinding
