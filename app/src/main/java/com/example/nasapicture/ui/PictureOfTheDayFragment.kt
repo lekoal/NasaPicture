14854dd7b0a3dec851b91ff280400ee96bd35985
lekoal <coreglus@gmail.com>
2022-03-11 15:00:02 +0300

    Motion layout is applied

--- app/src/main/java/com/example/nasapicture/ui/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/PictureOfTheDayFragment.kt
@@ -3,7 +3,9 @@ package com.example.nasapicture.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
