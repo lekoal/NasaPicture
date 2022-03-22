5cdb55f88dac6010ddde571164a8c6a4104de08b
lekoal <coreglus@gmail.com>
2022-03-22 16:21:46 +0300

    Transparent background is added

--- app/src/main/java/com/example/nasapicture/ui/AnimationFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/AnimationFragment.kt
@@ -1,7 +1,10 @@
 package com.example.nasapicture.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Property
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -30,20 +33,73 @@ class AnimationFragment : Fragment() {
         binding.fab.setOnClickListener {
             flag = !flag
             if (flag) {
-                ObjectAnimator.ofFloat(binding.fab, View.ROTATION, 0f, 405f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f, -250f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.SCALE_X, 0f, 1f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f, -160f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.SCALE_X, 0f, 1f).setDuration(duration).start()
-            } else {
-                ObjectAnimator.ofFloat(binding.fab, View.ROTATION, 405f, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -250f, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.SCALE_X, 1f, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -160f, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.SCALE_X, 1f, 0f).setDuration(duration).start()
+                objectAnimatorStart(binding.fab, View.ROTATION, 0f, 405f)
+                objectAnimatorStart(binding.optionOneContainer, View.TRANSLATION_Y, 0f, -250f)
+                objectAnimatorStart(binding.optionOneContainer, View.SCALE_X, 0f, 1f)
+                objectAnimatorStart(binding.optionTwoContainer, View.TRANSLATION_Y, 0f, -160f)
+                objectAnimatorStart(binding.optionTwoContainer, View.SCALE_X, 0f, 1f)
 
+                binding.transparentScreen.animate()
+                    .alpha(0.6f)
+                    .setDuration(duration)
+                    .start()
+
+                binding.optionOneContainer.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionOneContainer.isClickable = true
+                        }
+                    })
+
+                binding.optionTwoContainer.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionTwoContainer.isClickable = true
+                        }
+                    })
+            } else {
+                objectAnimatorStart(binding.fab, View.ROTATION, 405f, 0f)
+                objectAnimatorStart(binding.optionOneContainer, View.TRANSLATION_Y, -250f, 0f)
+                objectAnimatorStart(binding.optionOneContainer, View.SCALE_X, 1f, 0f)
+                objectAnimatorStart(binding.optionTwoContainer, View.TRANSLATION_Y, -160f, 0f)
+                objectAnimatorStart(binding.optionTwoContainer, View.SCALE_X, 1f, 0f)
+
+                binding.transparentScreen.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .start()
+
+                binding.optionOneContainer.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionOneContainer.isClickable = false
+                        }
+                    })
+
+                binding.optionTwoContainer.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionTwoContainer.isClickable = false
+                        }
+                    })
             }
         }
+    }
+
+    private fun objectAnimatorStart(view: View, property: Property<View, Float>, start: Float, end: Float) {
+        ObjectAnimator.ofFloat(view, property, start, end).setDuration(duration).start()
     }
 
     override fun onDestroy() {
