38f3f6cc2ffae2d8b1add0cdd16be0694d02abcd
lekoal <coreglus@gmail.com>
2022-03-25 14:05:35 +0300

    Add new items fab added

--- app/src/main/java/com/example/nasapicture/ui/RecyclerListFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/RecyclerListFragment.kt
@@ -1,6 +1,10 @@
 package com.example.nasapicture.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Property
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +19,10 @@ class RecyclerListFragment : Fragment() {
 
     private var _binding: FragmentRecyclerListBinding? = null
     private val binding get() = _binding!!
+
+    private var flag = false
+
+    private val duration = 1000L
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +54,32 @@ class RecyclerListFragment : Fragment() {
         }
         adapter.setPlanetListData(planetListData)
         binding.rvPlanets.adapter = adapter
+
+        binding.fabNewPlanetItem.setOnClickListener {
+            flag = !flag
+            if (flag) {
+                objectAnimatorStart(binding.fabNewPlanetItem, View.ROTATION, 0f, 405f)
+                objectAnimatorStart(binding.optionEarthContainer, View.TRANSLATION_Y, 0f, -250f)
+                objectAnimatorStart(binding.optionEarthContainer, View.SCALE_X, 0f, 1f)
+                objectAnimatorStart(binding.optionMarsContainer, View.TRANSLATION_Y, 0f, -160f)
+                objectAnimatorStart(binding.optionMarsContainer, View.SCALE_X, 0f, 1f)
+
+                viewAnimate(binding.transparentScreen, 0.6f, false)
+                viewAnimate(binding.optionEarthContainer, 1f, true)
+                viewAnimate(binding.optionMarsContainer, 1f, true)
+
+            } else {
+                objectAnimatorStart(binding.fabNewPlanetItem, View.ROTATION, 405f, 0f)
+                objectAnimatorStart(binding.optionEarthContainer, View.TRANSLATION_Y, -250f, 0f)
+                objectAnimatorStart(binding.optionEarthContainer, View.SCALE_X, 1f, 0f)
+                objectAnimatorStart(binding.optionMarsContainer, View.TRANSLATION_Y, -160f, 0f)
+                objectAnimatorStart(binding.optionMarsContainer, View.SCALE_X, 1f, 0f)
+
+                viewAnimate(binding.transparentScreen, 0f, false)
+                viewAnimate(binding.optionEarthContainer, 0f, true)
+                viewAnimate(binding.optionMarsContainer, 0f, true)
+            }
+        }
     }
 
     companion object {
@@ -53,6 +87,27 @@ class RecyclerListFragment : Fragment() {
         fun newInstance() = RecyclerListFragment()
     }
 
+    private fun objectAnimatorStart(
+        view: View,
+        property: Property<View, Float>,
+        start: Float,
+        end: Float
+    ) {
+        ObjectAnimator.ofFloat(view, property, start, end).setDuration(duration).start()
+    }
+
+    private fun viewAnimate(view: View, alpha: Float, needClickable: Boolean) {
+        view.animate()
+            .alpha(alpha)
+            .setDuration(duration)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    view.isClickable = needClickable && flag
+                }
+            })
+            .start()
+    }
 
     override fun onDestroy() {
         _binding = null
