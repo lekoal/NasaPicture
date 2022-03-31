c99ab00ff55d878670b24855b6ef0a42a0c4b917
lekoal <coreglus@gmail.com>
2022-03-31 12:14:55 +0300

    Splash picture animation added

--- app/src/main/java/com/example/nasapicture/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/SplashScreenFragment.kt
@@ -2,6 +2,8 @@ package com.example.nasapicture.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +12,25 @@ import com.example.nasapicture.databinding.FragmentSplashScreenBinding
 import com.example.nasapicture.tools.ViewBindingFragment
 
 @SuppressLint("CustomSplashScreen")
-class SplashScreenFragment : ViewBindingFragment<FragmentSplashScreenBinding>(FragmentSplashScreenBinding::inflate) {
+class SplashScreenFragment :
+    ViewBindingFragment<FragmentSplashScreenBinding>(FragmentSplashScreenBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.splashImage.animate().rotationBy(720f).setDuration(4000L).start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            requireActivity().supportFragmentManager.beginTransaction()
+                .setCustomAnimations(
+                    R.anim.slide_in,
+                    R.anim.fade_out,
+                    R.anim.fade_in,
+                    R.anim.slide_out
+                )
+                .replace(R.id.container, PictureOfTheDayFragment())
+                .commit()
+        }, 3000L)
     }
 
     companion object {
